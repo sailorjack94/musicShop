32e5d1557a3c6470946d4c9b9110e1b2608fb422
sailorj94 <jackniall9@gmail.com>
2021-06-18 13:37:12 +0100

    music shop logic working and tested

--- src/main/java/musicShop/MusicShop.java
+++ src/main/java/musicShop/MusicShop.java
@@ -1,4 +1,45 @@
 package musicShop;
 
+import behaviours.ISell;
+import instruments.Instrument;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MusicShop {
+
+    String name;
+    ArrayList<ISell> stock = new ArrayList<>();
+    int till;
+
+    public MusicShop(String name, ArrayList<ISell> stock, int till){
+        this.name = name;
+        this.stock = stock;
+        this.till = till;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getTill(){
+        return till;
+    }
+
+    public ArrayList<ISell> getStock(){
+        return stock;
+    }
+
+    public void addStock(Instrument instrument) {
+        stock.add(instrument);
+    }
+
+    public boolean stockCheck(Instrument instrument){
+        return stock.contains(instrument);
+    }
+
+    public void removeStock(Instrument instrument) {
+        if (stockCheck(instrument)){
+            stock.remove(instrument);
+        }
+    }
 }
--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,2 +1,67 @@
+import behaviours.ISell;
+import enums.KeyTypes;
+import instruments.bagpipes.HighlandBagpipes;
+import instruments.whistles.HighWhistle;
+import musicShop.Customer;
+import musicShop.MusicShop;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class MusicShopTest {
+
+    MusicShop shop;
+    HighWhistle whistle;
+    Customer customer;
+    ArrayList<ISell> stock;
+    HighlandBagpipes pipes;
+
+    @Before
+    public void before(){
+        stock = new ArrayList<>();
+        shop = new MusicShop("CodeClan Music Supplies", stock, 2500);
+        customer = new Customer("Niall", 150);
+        whistle = new HighWhistle("Whistle", 45, 70, "Aluminium", "High", KeyTypes.D);
+        pipes = new HighlandBagpipes("Bagpipes", 800, 1200, "Highland", KeyTypes.BFLAT, "Mahogany");
+    }
+
+    @Test
+    public void shopHasName(){
+        assertEquals("CodeClan Music Supplies", shop.getName());
+    }
+
+    @Test
+    public void shopStartsEmpty(){
+        assertEquals(0, shop.getStock().size());
+    }
+
+    @Test
+    public void shopHasTill(){
+        assertEquals(2500, shop.getTill());
+    }
+
+    @Test
+    public void canAddToShop(){
+        shop.addStock(whistle);
+        assertEquals(1, shop.getStock().size());
+    }
+
+    @Test
+    public void canRemoveStock(){
+        shop.addStock(whistle);
+        shop.addStock(whistle);
+        shop.removeStock(whistle);
+        assertEquals(1, shop.getStock().size());
+    }
+
+    @Test
+    public void unableToRemoveStockNotPresent(){
+        shop.addStock(whistle);
+        shop.removeStock(pipes);
+        assertEquals(1, shop.getStock().size());
+
+    }
 }
