9d0cb8e9eba268e13307a6840ab6466202fd9f75
sailorj94 <jackniall9@gmail.com>
2021-06-18 12:47:51 +0100

    High Whistle passing all test

--- src/main/java/instruments/whistles/HighWhistle.java
+++ src/main/java/instruments/whistles/HighWhistle.java
@@ -1,4 +1,17 @@
 package instruments.whistles;
 
-public class HighWhistle {
+import enums.KeyTypes;
+
+public class HighWhistle extends WhistleInstrument{
+
+    private final KeyTypes key;
+
+    public HighWhistle(String family, String body, String style, KeyTypes key){
+        super(family, body, style);
+        this.key = key;
+    }
+
+    public KeyTypes getKey(){
+        return key;
+    }
 }
--- src/test/java/HighWhistleTest.java
+++ src/test/java/HighWhistleTest.java
@@ -1,2 +1,38 @@
+import enums.KeyTypes;
+import instruments.strings.AcousticGuitar;
+import instruments.whistles.HighWhistle;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HighWhistleTest {
+
+    HighWhistle whistle;
+
+
+    @Before
+    public void before(){
+        whistle = new HighWhistle("Whistle", "Aluminium", "High", KeyTypes.D);
+    }
+
+    @Test
+    public void hasFamily() {
+        assertEquals("Whistle", whistle.getFamily());
+    }
+
+    @Test
+    public void hasStyle() {
+        assertEquals("High", whistle.getStyle());
+    }
+
+    @Test
+    public void hasBody(){
+        assertEquals("Aluminium", whistle.getBody());
+    }
+
+    @Test
+    public void hasKey(){
+        assertEquals(KeyTypes.D, whistle.getKey());
+    }
 }
