21c3fcde6aa22fd5df05331371e756666e43d4c8
sailorj94 <jackniall9@gmail.com>
2021-06-18 12:36:21 +0100

    Code tidied, tests running correctly

--- src/main/java/instruments/strings/StringedInstrument.java
+++ src/main/java/instruments/strings/StringedInstrument.java
@@ -3,4 +3,27 @@ package instruments.strings;
 import instruments.Instrument;
 
 public abstract class StringedInstrument extends Instrument {
+
+    private final int strings;
+    private final String body;
+    private final String style;
+
+    public StringedInstrument(String family, String style, int strings, String body){
+        super(family);
+        this.style = style;
+        this.strings = strings;
+        this.body = body;
+    }
+
+    public String getStyle(){
+        return style;
+
+    }
+    public int getStrings(){
+        return strings;
+    }
+
+    public String getBody(){
+        return body;
+    }
 }
--- src/main/java/instruments/whistles/WhistleInstrument.java
+++ src/main/java/instruments/whistles/WhistleInstrument.java
@@ -3,4 +3,21 @@ package instruments.whistles;
 import instruments.Instrument;
 
 public abstract class WhistleInstrument extends Instrument {
+
+    private final String body;
+    private final String style;
+
+    public WhistleInstrument(String family, String body, String style){
+        super(family);
+        this.body = body;
+        this.style = style;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getStyle() {
+        return style;
+    }
 }
